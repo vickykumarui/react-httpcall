889bf43d98a3aee2a6cecfbe7aebc63e909421ec
Vicky KUMAR <vicky.kumar@amadeus.com>
2020-09-18 15:53:19 +0530

    Get,post,delete request

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+
+    state = {
+        fullPost : null
+    }
+    deleteHandler = () =>{
+        axios.delete("https://jsonplaceholder.typicode.com/posts/"+this.state.fullPost.id).then((res) =>{
+            console.log(res);
+        })
+    }
+    componentDidUpdate(){
+        if(this.props.id){
+            
+            if(!this.state.fullPost || (this.state.fullPost && this.state.fullPost.id !== this.props.id)){
+                // do not update state if new data is not fetched because every update will trigger this lifecycle hook and cause infinite loop
+                // get backend data.
+                axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id).then((res) =>{
+                    this.setState({fullPost: res.data});
+                })
+            }
+        }
+    }
+    render () {
+        let post = <p>Please select a Post!</p>;
+        if(this.state.fullPost){
+        post = (
+            <div className="FullPost">
+                <h1>{this.state.fullPost.title}</h1>
+        <p>{this.state.fullPost.body}</p>
+                <div className="Edit">
+                    <button className="Delete" onClick={this.deleteHandler}>Delete</button>
+                </div>
+            </div>
+
+        );
+        }
+        return post;
+    }
+}
+
+export default FullPost;
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: [],
+        id: null,
+        showError: false
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((res) =>{
+            console.log(res);
+            const posts = res.data.slice(0,4);
+            const updatePosts = posts.map( post => {
+                return { ...post, author: 'Vicky'}; 
+            });
+            this.setState({posts: updatePosts});
+        })
+        .catch((err) =>{
+            console.log(err);
+            this.setState({showError: true, posts: []});
+        });
+    }
+
+    getPostDetailsHandler = (id) => {
+        console.log(id);
+        // axios.get('https://jsonplaceholder.typicode.com/posts/'+id).then((res) =>{
+        //     console.log(res);
+        //     this.setState({fullPost: res.data});
+        // });
+
+        this.setState({id: id});
+    }
+    render () {
+
+        let posts = <p>Sorry couldnot retrieve post</p>
+        if(!this.state.showError){
+            posts = this.state.posts.map((post) => <Post key = {post.id} id = {post.id} getPostDetailsHandler = {this.getPostDetailsHandler} title = {post.title} author = {post.author} />)
+        }
+      
+
+        
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                   <FullPost id = {this.state.id} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
