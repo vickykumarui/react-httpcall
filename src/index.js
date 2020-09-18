b18057c7c5227645e7db0dfcadd56fc67e389ec7
Vicky KUMAR <vicky.kumar@amadeus.com>
2020-09-18 16:14:01 +0530

    Adding interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use((request) =>{
+    console.log('inside interceptors', request);
+    // edit request - example add common header
+    return request;
+});
+
+axios.interceptors.response.use((response) =>{
+    console.log(response);
+    return response;
+}, (err) =>{
+    console.log(err);
+    return Promise.reject(err);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
